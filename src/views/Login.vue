e4089be2698b81db839bca9da59c6751e5f3ecbe
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-05-18 15:15:01 +0800

    perf(login): use navtive form check

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -86,57 +86,56 @@ onMounted(() => {
 			<CardTitle>登录到 CloudNotFlare</CardTitle>
 			<CardDescription></CardDescription>
 		</CardHeader>
-		<CardContent>
-			<Alert v-if="status.success" class="mb-4">
-				<CheckCircle2Icon />
-				<AlertTitle>登录成功!</AlertTitle>
-				<AlertDescription>
-					正在重定向...
-				</AlertDescription>
-			</Alert>
-			<Alert v-if="status.fail" variant="destructive" class="mb-4">
-				<AlertCircleIcon />
-				<AlertTitle>登录失败</AlertTitle>
-				<AlertDescription>
-					<p>请稍后再试。</p>
-					<ul class="mt-2 list-inside list-disc space-y-1">
-						<li>检查互联网连接</li>
-						<li>确保 Cloudflare 账户已启用 API 访问</li>
-						<li>确保填写的区域 ID 和 API 令牌是正确的</li>
-					</ul>
-				</AlertDescription>
-			</Alert>
-			<form>
-				<div class="grid w-full items-center gap-4">
-					<div class="flex flex-col space-y-1.5">
-						<Label for="zone-id">区域 ID</Label>
-						<Input id="zone-id" type="text" autocomplete="off" required v-model="form.zoneId" @change="status.empty = false" />
-					</div>
-					<div class="flex flex-col space-y-1.5">
-						<div class="flex items-center">
-							<Label for="api-token">API 令牌</Label>
-							<a href="https://developers.cloudflare.com/fundamentals/api/get-started/create-token/"
-								class="ml-auto inline-block text-sm underline" target="_blank" rel="noopener">
-								没有 API 令牌?
-							</a>
+		<form @submit.prevent="login">
+			<CardContent>
+				<Alert v-if="status.success" class="mb-4">
+					<CheckCircle2Icon />
+					<AlertTitle>登录成功!</AlertTitle>
+					<AlertDescription>
+						正在重定向...
+					</AlertDescription>
+				</Alert>
+				<Alert v-if="status.fail" variant="destructive" class="mb-4">
+					<AlertCircleIcon />
+					<AlertTitle>登录失败</AlertTitle>
+					<AlertDescription>
+						<p>请稍后再试。</p>
+						<ul class="mt-2 list-inside list-disc space-y-1">
+							<li>检查互联网连接</li>
+							<li>确保 Cloudflare 账户已启用 API 访问</li>
+							<li>确保填写的区域 ID 和 API 令牌是正确的</li>
+						</ul>
+					</AlertDescription>
+				</Alert>
+					<div class="grid w-full items-center gap-4">
+						<div class="flex flex-col space-y-1.5">
+							<Label for="zone-id">区域 ID</Label>
+							<Input id="zone-id" type="text" autocomplete="off" required v-model="form.zoneId" />
 						</div>
-						<Input id="api-token" type="password" placeholder="cfut_********************" autocomplete="off"
-							required class="font-mono" v-model="form.apiToken" @change="status.empty = false" />
+						<div class="flex flex-col space-y-1.5">
+							<div class="flex items-center">
+								<Label for="api-token">API 令牌</Label>
+								<a href="https://developers.cloudflare.com/fundamentals/api/get-started/create-token/"
+									class="ml-auto inline-block text-sm underline" target="_blank" rel="noopener">
+									没有 API 令牌?
+								</a>
+							</div>
+							<Input id="api-token" type="password" placeholder="cfut_********************" autocomplete="off"
+								required class="font-mono" v-model="form.apiToken" />
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<p class="text-sm">CloudNotFlare 承诺<b class="font-bold">不会</b>以任何方式收集你的 API 令牌或泄露给任何第三方。</p>
+						</div>
 					</div>
-					<div class="flex flex-col space-y-1.5">
-						<p class="text-sm">CloudNotFlare 承诺<b class="font-bold">不会</b>以任何方式收集你的 API 令牌或泄露给任何第三方。</p>
-					</div>
-				</div>
-			</form>
-		</CardContent>
-		<CardFooter class="flex flex-col gap-2">
-			<Button class="w-full" :class="{ 'animate-shake-x': status.empty }"
-				:disabled="status.logging || status.empty" @click="login">
-				<Loader2Icon v-if="status.logging" class="animate-spin" /> 登录
-			</Button>
-			<Button variant="outline" class="w-full" disabled>
-				通过 Cloudflare 登录
-			</Button>
-		</CardFooter>
+			</CardContent>
+			<CardFooter class="flex flex-col gap-2">
+				<Button type="submit" class="w-full" :disabled="status.logging">
+					<Loader2Icon v-if="status.logging" class="animate-spin" /> 登录
+				</Button>
+				<Button type="button" variant="outline" class="w-full" disabled>
+					通过 Cloudflare 登录
+				</Button>
+			</CardFooter>
+		</form>
 	</Card>
 </template>
